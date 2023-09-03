264db517c659ab67b912102b2936bb4780bd13ef
afadilla13 <128363337+afadilla13@users.noreply.github.com>
2023-09-03 15:46:49 +1000

    change the color

--- static/js/Bonus.js
+++ static/js/Bonus.js
@@ -70,22 +70,22 @@ d3.json(url).then(function(data) {
               type: "indicator",
               mode: "gauge+number",
               gauge: {
-                  axis: {range: [0,10], tickmode: "linear", tick0: 2, dtick: 2},
-                  bar: {color: "black"},
-                  steps: [
-                      {range: [0, 1], color: "rgba(255, 255, 255, 0)"},
-                      {range: [1, 2], color: "rgba(232, 226, 202, .5)"},
-                      {range: [2, 3], color: "rgba(210, 206, 145, .5)"},
-                      {range: [3, 4], color:  "rgba(202, 209, 95, .5)"},
-                      {range: [4, 5], color:  "rgba(184, 205, 68, .5)"},
-                      {range: [5, 6], color: "rgba(170, 202, 42, .5)"},
-                      {range: [6, 7], color: "rgba(142, 178, 35 , .5)"},
-                      {range: [7, 8], color:  "rgba(110, 154, 22, .5)"},
-                      {range: [8, 9], color: "rgba(50, 143, 10, 0.5)"},
-                      {range: [9, 10], color: "rgba(14, 127, 0, .5)"},
-                  ]
-              } 
-          };
+                axis: { range: [0, 10], tickmode: "linear", tick0: 2, dtick: 2 },
+                bar: { color: "darkblue" }, // Change "black" to "darkblue" for better visibility
+                steps: [
+                    { range: [0, 1], color: "rgba(255, 255, 255, 0)" },
+                    { range: [1, 2], color: "rgba(232, 226, 202, .5)" },
+                    { range: [2, 3], color: "rgba(210, 206, 145, .5)" },
+                    { range: [3, 4], color: "rgba(202, 209, 95, .5)" },
+                    { range: [4, 5], color: "rgba(184, 205, 68, .5)" },
+                    { range: [5, 6], color: "rgba(170, 202, 42, .5)" },
+                    { range: [6, 7], color: "rgba(142, 178, 35 , .5)" },
+                    { range: [7, 8], color: "rgba(110, 154, 22, .5)" },
+                    { range: [8, 9], color: "rgba(50, 143, 10, 0.5)" },
+                    { range: [9, 10], color: "rgba(14, 127, 0, .5)" },
+                ]
+            }
+        };
   
           // Define the layout settings for the gauge chart
           let layout = {
